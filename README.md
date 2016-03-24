12bc6e287200590ee4e0e1796c6d4bb45c0c8d4f
huhushow <huhushow@gmail.com>
2016-03-24 12:30:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+monolog-azuretable
+=============
+
+Azure Table Storage Handler for Monolog, which allows to store log messages in a Azure Table Storage.
+
+# Installation
+monolog-mysql is available via composer. Just add the following line to your required section in composer.json and do a `php composer.phar update`.
+
+```
+"huhushow/monolog-azuretable": ">1.0.0"
+```
+
+# Usage
+Just use it as any other Monolog Handler, push it to the stack of your Monolog Logger instance. The Handler however needs some parameters:
+
+- **$client** TableRestProxy Instance of your Azure Storage account.
+- **$table** The table name where the logs should be stored
+- **$level** can be any of the standard Monolog logging levels. Use Monologs statically defined contexts. _Defaults to Logger::DEBUG_
+- **$bubble** _Defaults to true_
+
+# Examples
+Given that $client is your TableRestProxy instance, you could use the class as follows:
+
+```php
+//Import class
+use AzureTableStorageHandler\AzureTableStorageHandler;
+
+//Create MysqlHandler
+$AzureTableStorageHandler = new AzureTableStorageHandler($client, "log", \Monolog\Logger::DEBUG);
+
+//Create logger
+$logger = new \Monolog\Logger($context);
+$logger->pushHandler($AzureTableStorageHandler);
+
+//Now you can use the logger, and further attach additional information
+```
+
+# License
+This tool is free software and is distributed under the MIT license. Please have a look at the LICENSE file for further information.
---
+++ src/AzureTableStorageHandler.php
@@ -0,0 +1,113 @@
+<?php
+
+/* 
+ * 
+ * 
+ * 
+ */
+
+namespace Huhushow\AzureTableStorageHandler;
+
+use WindowsAzure\Table\TableRestProxy;
+use WindowsAzure\Table\Models\Entity;
+use WindowsAzure\Common\ServiceException;
+use WindowsAzure\Table\Models\EdmType;
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Formatter\ScalarFormatter;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+class AzureTableStorageHandler extends AbstractProcessingHandler
+{
+	const DATE_FORMAT = 'Y-m-d\TH:i:s.uO';
+
+	/**
+	 * @var AzureTableRestProxy Service Builder
+	 */
+	protected $client;
+
+	/**
+	 * @var string
+	 */
+	protected $table;
+	/**
+	 * @param AzureTableClient $client
+	 * @param string           $table
+	 * @param int              $level
+	 * @param bool             $bubble
+	*/
+	public function __construct(TableRestProxy $client, $table, $level = Logger::DEBUG, $bubble = true)
+	{
+			$this->client = $client;
+			$this->table = $table;
+			try {
+				// Create table.
+				$this->client->createTable($this->table);
+			} catch(ServiceException $e) {
+				$eMsg = $e->getMessage();
+				$startPos = strpos($eMsg, '<code>') + 6;
+				$endPos = strpos($eMsg, '</code>');
+				$eCode = substr($eMsg, $startPos, ($endPos - $startPos));
+				if ($eCode == 'TableAlreadyExists'){
+				} else {
+					echo $eCode;
+				}
+			}
+			parent::__construct($level, $bubble);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function write(array $record)
+	{
+			$filtered = json_decode($record['formatted'],true);
+			$result = $this->flatten($filtered);
+			$entity = new Entity();
+			$entity->setPartitionKey($result['message'].'-'.$_SERVER['SERVER_NAME']);
+			$entity->setRowKey(strval(PHP_INT_MAX - ((int)(microtime(true)*10000))));
+			foreach ($result as $k => $v) {
+				$entity->addProperty(strval($k), null, strval($v));
+			}
+			$this->client->insertOrReplaceEntity($this->table, $entity);
+	}
+	/**
+	 * @param  array $record
+	 * @return array
+	 */
+	protected function filterEmptyFields(array $record)
+	{
+			return array_filter($record, function ($value) {
+					return !empty($value) || false === $value || 0 === $value;
+			});
+	}
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function getDefaultFormatter()
+	{
+			return new JsonFormatter(self::DATE_FORMAT, JsonFormatter::BATCH_MODE_JSON, false);
+	}
+
+	protected function flattenInner(array &$out, array $arr, $prefix)
+	{
+			foreach ($arr as $k => $v) {
+					$key = (!strlen($prefix)) ? $k : "{$prefix}__{$k}";
+					if (is_array($v)) {
+						$this->flattenInner($out, $v, $key);
+					} else {
+							$out[$key] = $v;
+					}
+			}
+	}
+
+	protected function flatten(array $arr)
+	{
+
+			$flat = array();
+			$this->flattenInner($flat, $arr, '');
+			return $flat;
+
+	}
+}
